79e189ba95ebc86ef464fdd8546f079ac6d82ad2
Veki22 <111987930+Veki22@users.noreply.github.com>
2022-10-25 22:51:01 +0200

    Update 1.3_Energiequellen_GHE.py

--- Skripte/1.3_Energiequellen_GHE.py
+++ Skripte/1.3_Energiequellen_GHE.py
@@ -17,19 +17,6 @@
 
 # ### Notebook vorbereiten und die benötigten Daten aus dem GWR einlesen
 
-# In[1]:
-
-
-#import pandas as pd
-#import geopandas as gpd
-
-#lesen GWR Daten
-#dfGWRSource = pd.read_csv('GWR_Data/gebaeude_batiment_edificio_CH.csv', usecols=['GGDENAME','GKODE', 'GKODN', 'GENH1'],  sep='\t')
-
-
-# In[2]:
-
-
 import pandas as pd
 import geopandas as gpd
 from io import BytesIO
@@ -46,9 +33,6 @@ dfGWRSource = pd.read_csv(zipfile.open('gebaeude_batiment_edificio.csv'),
                          sep='\t')
 
 
-# In[3]:
-
-
 #Column mit geeigneten Namen
 dfGWRSource = dfGWRSource.rename(columns={"GGDENAME": "Gemeinde",
                                           "GKODE": "lat",
@@ -56,17 +40,13 @@ dfGWRSource = dfGWRSource.rename(columns={"GGDENAME": "Gemeinde",
                                           "GENH1":"Energiequelle"
                                           })
 
-
-# In[4]:
-
-
-dfGWRSource
-
-
-# In[5]:
-
-
-# Die korrekten Bezeichnungen den Heizcodes zuweisen und als Liste speichern.
+# Die Bezeichnungen den Heizcodes zuweisen und als Liste speichern.
+# Um die finale Visualisierung auf die drei definierten Energiequellen (Gas, Heizöl, Elektrizität) zu fokussieren, werden die Energiequellen zusammengefasst.
+# Im GWR-Datenfile sind folgende Energiequellen definiert:
+# 7501: 'Luft', 7510: 'Erdwärme (generisch', 7511: 'Erdwärmesonde', 7512: 'Erdregister', 7513: 'Wasser (Grundwasser, Oberflächenwasser, Abwasser)'
+# 7520: 'Gas', 7530: 'Heizöl', 7540: 'Holz (generisch)', 7541: 'Holz (Stückholz)', 7542: 'Holz (Pellets)', 7543: 'Holz (Schnitzel)', 7550: 'Abwärme (innerhalb des Gebäudes)'
+# 7560: 'Elektrizität', 7570: 'Sonne (thermisch)', 7580: 'Fernwärme (generisch)', 7581: 'Fernwärme (Hochtemperatur)', 7582: 'Fernwärme (Niedertemperatur)'
+# 7598: 'Unbestimmt', 7599: 'Andere'
 dfGWRSource['Energiequelle'] = dfGWRSource.Energiequelle.replace({
                                             7500: 'Keine',
                                             7501: 'Luft',
@@ -87,54 +67,20 @@ dfGWRSource['Energiequelle'] = dfGWRSource.Energiequelle.replace({
                                             7581: 'Ferwärme',
                                             7582: 'Fernwärme',
                                             7598: 'Unbestimmt',
-                                            7599: 'Keine'
+                                            7599: 'Weitere'
                                             })
-
-
-# In[6]:
-
-
-dfGWRSource
-
-
-# In[7]:
 
 
 #nur die fossilen Energiequellen filtern
 options = ['Gas', 'Heizöl', 'Elektrizität']
 dfGWRfossileEnergiequelle = dfGWRSource[(dfGWRSource['Energiequelle'].isin(options))] 
 
-dfGWRfossileEnergiequelle
-
-                                                 
-
-
-# In[8]:
-
-
-#Nullwerte anschauen
-dfGWRfossileEnergiequelle_null = dfGWRfossileEnergiequelle.isnull().sum()
-dfGWRfossileEnergiequelle_null
-
 
 # ### Daten als Geopackage zur Weiterverarbeitung bzw. Visusliserung exportieren
-
-# In[9]:
-
 
 gdf = gpd.GeoDataFrame(
     dfGWRfossileEnergiequelle, geometry=gpd.points_from_xy(dfGWRfossileEnergiequelle.lat, dfGWRfossileEnergiequelle.lon)
 )
-
-
-# In[10]:
-
-
-gdf
-
-
-# In[11]:
-
 
 #Datenframe für Verteilung der Energietrager pro Gemeinde
 dfEnergyHeatmap = gdf[['Gemeinde','Energiequelle', 'geometry']]
@@ -142,23 +88,8 @@ dfEnergyHeatmap
 
 
 # ### Daten in ein GeoPackage-File exportieren
-
-# In[11]:
-
-
 #Als GeoPackage-File exportieren
 #gdf.to_file('Export_Data/GeoPackage/EnergiequelleFossile.gpkg', driver='GPKG', limit=0)
-
-
-# In[12]:
-
-
-#Als GeoPackage-File exportieren
-gdf.to_file('Daten/EnergiequelleFossile.gpkg', driver='GPKG', limit=0)
-
-
-# In[ ]:
-
-
+gdf.to_file('Daten/Gemeindeliste_1-3.gpkg', driver='GPKG', limit=0)
 
 
