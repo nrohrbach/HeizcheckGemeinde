bef5198a7b7ac341e5a13da7d0d7849108a5ab08
nrohrbach <52918447+nrohrbach@users.noreply.github.com>
2022-10-26 16:32:54 +0200

    Update 1.3_Energiequellen_GHE.py

--- Skripte/1.3_Energiequellen_GHE.py
+++ Skripte/1.3_Energiequellen_GHE.py
@@ -1,22 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Fragestellung 1.3: -> Export nur Energiequellen GAS - HEIZÖL - ELEKTRIZITÄT
-# ## Welche Gebiete in einer Gemeinde sind am stärksten betroffen?
-# 
-# Um diese Frage zu beantworten, werden Daten aus dem "Gebäude- und Wohnungsregister" (GWR) verwendet. <br>
-# [Link zum GWR](https://www.housing-stat.ch/de/index.html)<br>
-# [Link zur GWR Dokumentation](https://www.housing-stat.ch/de/docs/index.html)<br>
-# [Link zum GWR Download](https://www.housing-stat.ch/de/madd/public.html)<br>
-# <br>
-# Die Daten werden nach Gemeinde und Energiequelle gruppiert und dann die einzelnen Objekte gezählt. <br>
-# Anschliessend wird die resultierende Liste als Geopackage zur weiterverarbeitung bzw. Visualisierung exportiert.<br>
-# <br>
-# ---
-# <i> CAS Spatial Data Analytics 2022 </i> ¦ <i> Kommunale Übersicht von Heizsystemen und Energieträgern in Wohngebäuden </i> ¦ <i> Stand: 22.09.2022  </i> ¦ <i> Entwickler: Jürg Reist </i>
-
-# ### Notebook vorbereiten und die benötigten Daten aus dem GWR einlesen
-
 import pandas as pd
 import geopandas as gpd
 from io import BytesIO
@@ -40,13 +21,7 @@ dfGWRSource = dfGWRSource.rename(columns={"GGDENAME": "Gemeinde",
                                           "GENH1":"Energiequelle"
                                           })
 
-# Die Bezeichnungen den Heizcodes zuweisen und als Liste speichern.
-# Um die finale Visualisierung auf die drei definierten Energiequellen (Gas, Heizöl, Elektrizität) zu fokussieren, werden die Energiequellen zusammengefasst.
-# Im GWR-Datenfile sind folgende Energiequellen definiert:
-# 7501: 'Luft', 7510: 'Erdwärme (generisch', 7511: 'Erdwärmesonde', 7512: 'Erdregister', 7513: 'Wasser (Grundwasser, Oberflächenwasser, Abwasser)'
-# 7520: 'Gas', 7530: 'Heizöl', 7540: 'Holz (generisch)', 7541: 'Holz (Stückholz)', 7542: 'Holz (Pellets)', 7543: 'Holz (Schnitzel)', 7550: 'Abwärme (innerhalb des Gebäudes)'
-# 7560: 'Elektrizität', 7570: 'Sonne (thermisch)', 7580: 'Fernwärme (generisch)', 7581: 'Fernwärme (Hochtemperatur)', 7582: 'Fernwärme (Niedertemperatur)'
-# 7598: 'Unbestimmt', 7599: 'Andere'
+
 dfGWRSource['Energiequelle'] = dfGWRSource.Energiequelle.replace({
                                             7500: 'Keine',
                                             7501: 'Luft',
@@ -84,7 +59,6 @@ gdf = gpd.GeoDataFrame(
 
 #Datenframe für Verteilung der Energietrager pro Gemeinde
 dfEnergyHeatmap = gdf[['Gemeinde','Energiequelle', 'geometry']]
-dfEnergyHeatmap
 
 
 # ### Daten in ein GeoPackage-File exportieren
