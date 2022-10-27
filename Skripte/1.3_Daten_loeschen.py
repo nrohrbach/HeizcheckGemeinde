0a90118e54c0becc8baaa535a768e0c4ffd8dea2
Veki22 <111987930+Veki22@users.noreply.github.com>
2022-10-27 23:40:57 +0200

    Create 1.3_Daten_loeschen.py

---
+++ Skripte/1.3_Daten_loeschen.py
@@ -0,0 +1,10 @@
+# Datei vorher löschen
+import os
+if os.path.exists('Daten/Gemeindeliste_1-3_part1.gpkg'):
+  os.remove('Daten/Gemeindeliste_1-3_part1.gpkg')
+  
+# Datei vorher löschen
+if os.path.exists('Daten/Gemeindeliste_1-3_part2.gpkg'):
+  os.remove('Daten/Gemeindeliste_1-3_part2.gpkg')
+  
+  
